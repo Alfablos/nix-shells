157c934e124ac2542f1b6a68204d4b48d7f5a0fd
Antonio Galanti <antonio.galanti@gmail.com>
2025-09-15 21:06:03 +0200

    updated rust with stable and nightly; version bump; default is 'latest' for stable and 'unstable'

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       url = "github:nix-community/crate2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
+    # git-hooks.url = "github:cachix/git-hooks.nix";
   };
 
   outputs =
@@ -19,27 +19,38 @@
       nixpkgs,
       crate2nix,
       oxalica-rust,
+      # git-hooks
     }:
     let
       forAllSystems = f:
       nixpkgs.lib.genAttrs [
         "x86_64-linux"
         "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
       ] (system: f (import nixpkgs { inherit system; config.allowUnfree = true; overlays = []; }));
 
     in
     {
       devShells = forAllSystems (pkgs: {
         default = self.devShells.${pkgs.system}.rust;
-        rust = self.packages.${pkgs.system}.lib.shells.rust { };
+        rust-stable = (self.packages.${pkgs.system}.lib.shells.rust { }).stable;
+        rust-nightly = (self.packages.${pkgs.system}.lib.shells.rust { }).nightly;
       });
 
-      packages = forAllSystems (pkgs: {
-        lib.shells.rust = { rustVersion ? "1.84.0", withPkgs ? [ ], additionalLibraryPaths ? null }:
+      packages = forAllSystems (pkgs:
+      let
+        commonPackages = with pkgs; [
+          git bat fd ripgrep just gnumake neovim
+          procs bottom hexyl hyperfine tokei
+        ];
+      in
+      {
+        lib.shells.rust = { rustVersionStable ? "latest", rustVersionNightly ? "latest", withPkgs ? [ ], additionalLibraryPaths ? null }:
           pkgs.callPackage ./rust.nix {
             pkgs = import nixpkgs { system = pkgs.system; overlays = [ (import oxalica-rust) ]; };
             crate2nix = crate2nix.packages.${pkgs.system}.default;
-            inherit rustVersion withPkgs;
+            inherit rustVersionStable rustVersionNightly withPkgs commonPackages;
         };
         lib.configs.neovim.rust = {}; # TODO
       });
